e04236f500f1e865d21592e5cbfb8e7bd749edfe
Simo02xxx <simokardoudi3@email.com>
2025-05-25 15:44:00 +0100

    Connexion à la base PostgreSQL Railway

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
+from config import Config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
@@ -9,7 +10,7 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('../config.py')
+    app.config.from_object(Config)  # Charge Config depuis config.py
 
     db.init_app(app)
     csrf.init_app(app)
