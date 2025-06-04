1fd1b017ca66f6fe307ce602e37b5454d3f3669d
Simo02xxx <simokardoudi3@email.com>
2025-06-04 21:40:01 +0100

    reset password

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
     MAIL_SERVER = 'smtp.gmail.com'
-    
+
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'ton.email@gmail.com'    # ton email d’envoi
-    MAIL_PASSWORD = 'ton_mot_de_passe_app'   # mot de passe ou mot de passe d’application
+    MAIL_USERNAME = ''    # ton email d’envoi
+    MAIL_PASSWORD = ''   # mot de passe ou mot de passe d’application
     SECRET_KEY = 'une_clef_secrete_pour_token'  # déjà présent normalement
